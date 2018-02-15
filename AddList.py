0cf221414c344722bb0ed5131a564631ae2e8afb
Shouma Kawakami <e155705@ie.u-ryukyu.ac.jp>
2018-02-16 02:05:24 +0900

    addListmodule

--- AddList.py
+++ AddList.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def addList(arg1,arg2):
     total = arg1 + arg2
     return(total)
@@ -6,3 +8,14 @@ def addList(arg1,arg2):
 totalList = addList(10,20)
 
 print(totalList)
+
+x =  np.array([1,2,3,4])
+y = np.array([2,3,4])
+
+LenX = len(x)
+LenY = len(y)
+
+if LenX == LenY:
+    print(x + y)
+else:
+    print('None')
